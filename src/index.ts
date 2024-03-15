3dd263203f8e967380172d72fcf0f59b8872c2d2
Joseph Mearman <joseph@mearman.co.uk>
2024-03-15 16:25:17 +0000

    Refactor color and node types in JSONCanvas

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,28 @@ export type JSONCanvasColor = string | APresetColor;
  * 5 cyan
  * 6 purple
  */
-export type APresetColor = "1" | "2" | "3" | "4" | "5" | "6";
+export const APresetColor = {
+	Red: "1",
+	Orange: "2",
+	Yellow: "3",
+	Green: "4",
+	Cyan: "5",
+	Purple: "6",
+} as const;
+export type APresetColor = (typeof APresetColor)[keyof typeof APresetColor];
+
 /**
  * The rendering style of the end of the edge line
  */
 export type JSONCanvasEdgeEnd = "none" | "arrow";
+
 export type JSONCanvasNode =
 	| JSONCanvasNodeGeneric
 	| JSONCanvasTextNode
 	| JSONCanvasFileNode
 	| JSONCanvasLinkNode
 	| JSONCanvasGroupNode;
+
 /**
  * Options are:
  * cover - fills the entire width and height of the node.
@@ -38,6 +49,8 @@ export interface JsonCanvas {
 	nodes?: JSONCanvasNode[];
 	[k: string]: unknown;
 }
+type Side = "top" | "right" | "bottom" | "left";
+
 export interface JSONCanvasEdge {
 	color?: JSONCanvasColor;
 	/**
@@ -47,7 +60,7 @@ export interface JSONCanvasEdge {
 	/**
 	 * The side of the node that the edge connects from
 	 */
-	fromSide?: "top" | "right" | "bottom" | "left";
+	fromSide?: Side;
 	/**
 	 * The ID for the edge
 	 */
@@ -56,6 +69,9 @@ export interface JSONCanvasEdge {
 	 * The text label for the edge
 	 */
 	label?: string;
+	/**
+	 * The rendering style of the end of the edge line
+	 */
 	toEnd?: JSONCanvasEdgeEnd;
 	/**
 	 * The ID of the node that the edge ends at
@@ -64,9 +80,22 @@ export interface JSONCanvasEdge {
 	/**
 	 * The side of the node that the edge connects to
 	 */
-	toSide?: "top" | "right" | "bottom" | "left";
+	toSide?: Side;
 }
+
+export const NodeType = {
+	Text: "text",
+	File: "file",
+	Link: "link",
+	Group: "group",
+} as const;
+
+export type NodeType = (typeof NodeType)[keyof typeof NodeType];
+
 export interface JSONCanvasNodeGeneric {
+	/**
+	 * The color of the node
+	 */
 	color?: JSONCanvasColor;
 	/**
 	 * The height of the node in pixels
@@ -79,7 +108,7 @@ export interface JSONCanvasNodeGeneric {
 	/**
 	 * The node type
 	 */
-	type: "text" | "file" | "link" | "group";
+	type: NodeType;
 	/**
 	 * The width of the node in pixels
 	 */
@@ -93,133 +122,43 @@ export interface JSONCanvasNodeGeneric {
 	 */
 	y: number;
 }
-export interface JSONCanvasTextNode {
-	color?: JSONCanvasColor;
-	/**
-	 * The height of the node in pixels
-	 */
-	height: number;
-	/**
-	 * Unique ID for the node
-	 */
-	id: string;
+export interface JSONCanvasTextNode extends JSONCanvasNodeGeneric {
+	type: typeof NodeType.Text;
 	/**
 	 * Plain text with Markdown syntax
 	 */
 	text: string;
-	/**
-	 * The node type
-	 */
-	type: "text";
-	/**
-	 * The width of the node in pixels
-	 */
-	width: number;
-	/**
-	 * The x position of the node in pixels
-	 */
-	x: number;
-	/**
-	 * The y position of the node in pixels
-	 */
-	y: number;
 }
-export interface JSONCanvasFileNode {
-	color?: JSONCanvasColor;
+export interface JSONCanvasFileNode extends JSONCanvasNodeGeneric {
+	type: typeof NodeType.File;
 	/**
 	 * The path to the file within the system
 	 */
 	file: string;
 	/**
-	 * The height of the node in pixels
-	 */
-	height: number;
-	/**
-	 * Unique ID for the node
-	 */
-	id: string;
-	/**
 	 * The subpath that may link to a heading or a block. Always starts with a #
 	 */
 	subpath?: string;
-	/**
-	 * The node type
-	 */
-	type: "file";
-	/**
-	 * The width of the node in pixels
-	 */
-	width: number;
-	/**
-	 * The x position of the node in pixels
-	 */
-	x: number;
-	/**
-	 * The y position of the node in pixels
-	 */
-	y: number;
 }
-export interface JSONCanvasLinkNode {
-	color?: JSONCanvasColor;
+export interface JSONCanvasLinkNode extends JSONCanvasNodeGeneric {
+	type: typeof NodeType.Link;
 	/**
-	 * The height of the node in pixels
+	 * The URL to link to
 	 */
-	height: number;
-	/**
-	 * Unique ID for the node
-	 */
-	id: string;
-	/**
-	 * The node type
-	 */
-	type: "link";
 	url: string;
-	/**
-	 * The width of the node in pixels
-	 */
-	width: number;
-	/**
-	 * The x position of the node in pixels
-	 */
-	x: number;
-	/**
-	 * The y position of the node in pixels
-	 */
-	y: number;
 }
-export interface JSONCanvasGroupNode {
+export interface JSONCanvasGroupNode extends JSONCanvasNodeGeneric {
+	type: typeof NodeType.Group;
 	/**
 	 * The path to the background image
 	 */
 	background?: string;
+	/**
+	 * The rendering style of the background image
+	 */
 	backgroundStyle?: TheRenderingStyleOfABackgroundImage;
-	color?: JSONCanvasColor;
-	/**
-	 * The height of the node in pixels
-	 */
-	height: number;
-	/**
-	 * Unique ID for the node
-	 */
-	id: string;
 	/**
 	 * The text label for the group
 	 */
 	label?: string;
-	/**
-	 * The node type
-	 */
-	type: "group";
-	/**
-	 * The width of the node in pixels
-	 */
-	width: number;
-	/**
-	 * The x position of the node in pixels
-	 */
-	x: number;
-	/**
-	 * The y position of the node in pixels
-	 */
-	y: number;
 }
